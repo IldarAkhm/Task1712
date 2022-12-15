9f8dbdf5614fb5550c6116347d18c9b97468173e
IldarAkhm <ildar449@mail.ru>
2022-12-15 16:26:51 +0300

    firstcommit

---
+++ Tumak13/BuildCont.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumak13
+{
+    internal class BuildCont
+    {
+        Buiding[] buidings = new Buiding[10];
+        public Buiding this[int index]
+        {
+            get => buidings[index];
+            set => buidings[index] = value;
+        }
+        public BuildCont(Buiding[] buidings)
+        {
+            this.buidings = buidings;
+        }
+    }
+}
