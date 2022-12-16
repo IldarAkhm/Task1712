8bc0915d09da37af8473c8bcfbc661b1ec0e7ff2
IldarAkhm <ildar449@mail.ru>
2022-12-16 21:49:57 +0300

    lastcommit(mb)

---
+++ Tumak14/DeveloperInfoAttribute..cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumak14
+{
+    class DeveloperInfoAttribute : System.Attribute
+    {
+        public string Name { get; }
+        public string datereliz { get; }
+
+        public DeveloperInfoAttribute() { }
+        public DeveloperInfoAttribute(string name, string datereliz)
+        {
+            Name = name;
+            this.datereliz = datereliz;
+        }
+    }
+}
---
+++ Tumak14/NameAndOrgAtribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumak14
+{
+    class NameAndOrgAtribute : System.Attribute
+    {
+        public string Name { get; }
+        public string Organization { get; set; }
+        public NameAndOrgAtribute(string name, string organization)
+        {
+            Name = name;
+            Organization = organization;
+        }
+        public NameAndOrgAtribute() { }
+    }
+}
